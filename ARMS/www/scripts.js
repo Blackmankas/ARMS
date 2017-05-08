d63204fdcb5fbd81b6ba8cc71a1a2c6a6f696fa2
alecsloan <sloan.alec@gmail.com>
2017-05-07 20:03:27 -0700

    Added new scan html, minor changes to other files

--- ARMS/www/scripts.js
+++ ARMS/www/scripts.js
@@ -5,7 +5,7 @@ var app = {
 		
 			// This code runs if the scan succeeds
 			function (result) {
-				window.location.replace("home.html?barcode="+result.text);
+				window.location.replace("scan.html?barcode="+result.text);
 				//alert("We got a barcode\n" +
 						//"Result: " + result.text);
 			},
@@ -18,11 +18,11 @@ var app = {
 			// scanner configuration settings
 			{
 				//preferFrontCamera : true, // iOS and Android
-				showFlipCameraButton : true, // iOS and Android
+				//showFlipCameraButton : true, // iOS and Android
 				showTorchButton : true, // iOS and Android
 				torchOn: false, // Android, launch with the torch switched on (if available)
 				prompt : "Place a barcode inside the scan area", // Android
-				resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+				resultDisplayDuration: 0, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
 				//formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
 				//orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
 				disableAnimations : true, // iOS
